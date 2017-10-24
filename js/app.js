c2fc97f821b817218c8d0b7bfd746e60d065edfa
Patrick Glynn <pjglynn57@gmail.com>
2017-10-23 21:58:49 -0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,60 +1,96 @@
 (function() {
-  'use strict';
+'use strict';
 
-  const movies = [];
+var movies = [];
 
-  const renderMovies = function() {
-    $('#listings').empty();
+const renderMovies = function() {
+  $('#listings').empty();
 
-    for (const movie of movies) {
-      const $col = $('<div>').addClass('col s6');
-      const $card = $('<div>').addClass('card hoverable');
-      const $content = $('<div>').addClass('card-content center');
-      const $title = $('<h6>').addClass('card-title truncate');
+  for (const movie of movies) {
+    const $col = $('<div>').addClass('col s6');
+    const $card = $('<div>').addClass('card hoverable');
+    const $content = $('<div>').addClass('card-content center');
+    const $title = $('<h6>').addClass('card-title truncate');
 
-      $title.attr({
-        'data-position': 'top',
-        'data-tooltip': movie.title
-      });
+    $title.attr({
+      'data-position': 'top',
+      'data-tooltip': movie.title
+    });
 
-      $title.tooltip({ delay: 50 }).text(movie.title);
+    $title.tooltip({
+      delay: 50
+    }).text(movie.title);
 
-      const $poster = $('<img>').addClass('poster');
+    const $poster = $('<img>').addClass('poster');
 
-      $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
-      });
+    $poster.attr({
+      src: movie.poster,
+      alt: `${movie.poster} Poster`
+    });
 
-      $content.append($title, $poster);
-      $card.append($content);
+    $content.append($title, $poster);
+    $card.append($content);
 
-      const $action = $('<div>').addClass('card-action center');
-      const $plot = $('<a>');
+    const $action = $('<div>').addClass('card-action center');
+    const $plot = $('<a>');
 
-      $plot.addClass('waves-effect waves-light btn modal-trigger');
-      $plot.attr('href', `#${movie.id}`);
-      $plot.text('Plot Synopsis');
+    $plot.addClass('waves-effect waves-light btn modal-trigger');
+    $plot.attr('href', `#${movie.id}`);
+    $plot.text('Plot Synopsis');
 
-      $action.append($plot);
-      $card.append($action);
+    $action.append($plot);
+    $card.append($action);
 
-      const $modal = $('<div>').addClass('modal').attr('id', movie.id);
-      const $modalContent = $('<div>').addClass('modal-content');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+    const $modal = $('<div>').addClass('modal').attr('id', movie.id);
+    const $modalContent = $('<div>').addClass('modal-content');
+    const $modalHeader = $('<h4>').text(movie.title);
+    const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
+    const $modalText = $('<p>').text(movie.plot);
 
-      $modalContent.append($modalHeader, $movieYear, $modalText);
-      $modal.append($modalContent);
+    $modalContent.append($modalHeader, $movieYear, $modalText);
+    $modal.append($modalContent);
 
-      $col.append($card, $modal);
+    $col.append($card, $modal);
 
-      $('#listings').append($col);
+    $('#listings').append($col);
 
-      $('.modal-trigger').leanModal();
+    $('.modal-trigger').leanModal();
+  }
+};
+
+
+$('form').submit(function(event) {
+  event.preventDefault()
+});
+
+$('button').on('click', function() {
+  let $input = $('#search')[0].value;
+  if ($('#search')[0].value === "") {
+    alert("Please enter a valid movie");
+  }
+  $('#search')[0].value = "";
+
+  console.log($input);
+
+  var $xhr = $.getJSON("https:omdb-api.now.sh/?s="+$input);
+
+  $xhr.done(function(data) {
+    for (var i = 0; i < data.Search.length; i++) {
+      var obj = {
+        "poster": data.Search[i].Poster,
+        "id": data.Search[i].imdbID,
+        "title": data.Search[i].Title,
+        "year": data.Search[i].Year
+      }
+      movies.push(obj);
     }
-  };
 
-  // ADD YOUR CODE HERE
-})();
+
+    renderMovies();
+    movies = [];
+
+  });
+
+});
+
+});
